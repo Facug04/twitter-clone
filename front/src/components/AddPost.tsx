41dbe89e3c3fd9107055982ea75a73841d17073f
Facug03 <facundogonzales1550@gmail.com>
2023-02-24 13:13:14 -0300

    error hadling

--- front/src/components/AddPost.tsx
+++ front/src/components/AddPost.tsx
@@ -13,7 +13,6 @@ import Emoji from './icons/Emoji'
 import Program from './icons/Program'
 import Ubication from './icons/Ubication'
 import Twitter from './icons/Twitter'
-import SesionMobile from './SesionMobile'
 
 type Props = {
   user: boolean | undefined
@@ -23,6 +22,8 @@ type Props = {
 
 export default function AddPost({ user, currentUser, name }: Props) {
   const [selectedImage, setSelectedImage] = useState<File | null>(null)
+  const [isDisabled, setIsDisabled] = useState(true)
+  const [error, setError] = useState(false)
   const {
     register,
     formState: { errors },
@@ -43,29 +44,33 @@ export default function AddPost({ user, currentUser, name }: Props) {
   })
 
   const onSubmit = (data: FieldValues) => {
-    if (user) {
-      const newPost = {
-        ...data,
-        image: currentUser?.photoURL,
-        username: currentUser?.displayName,
-        commentImage: uploadImage(),
+    if (!name.isReady) setError(true)
+    else {
+      setError(false)
+      if (user && !isDisabled) {
+        const newPost = {
+          ...data,
+          image: currentUser?.photoURL,
+          username: currentUser?.displayName,
+          commentImage: uploadImage(),
+        }
+        mutate(newPost, {
+          onSuccess: () => {
+            reset()
+          },
+        })
+      } else if (name.isReady && !isDisabled) {
+        const newPost = {
+          ...data,
+          username: name.username,
+          commentImage: uploadImage(),
+        }
+        mutate(newPost, {
+          onSuccess: () => {
+            reset()
+          },
+        })
       }
-      mutate(newPost, {
-        onSuccess: () => {
-          reset()
-        },
-      })
-    } else if (name.isReady) {
-      const newPost = {
-        ...data,
-        username: name.username,
-        commentImage: uploadImage(),
-      }
-      mutate(newPost, {
-        onSuccess: () => {
-          reset()
-        },
-      })
     }
   }
 
@@ -89,35 +94,36 @@ export default function AddPost({ user, currentUser, name }: Props) {
       return `https://res.cloudinary.com/dlkdvbani/image/upload/v1676557124/${id}.png`
     } else ''
   }
-
+  console.log({ isError })
   return (
-    <div className='pt-3 border-[#2f3336] border-x-[1.5px]'>
-      <div className='px-4'>
-        <h2 className='text-normal font-chirp-bold text-pri mb-7 max-[500px]:hidden'>
-          Inicio
-        </h2>
-        <div className='fill-primary flex justify-center mb-6'>
-          <Twitter size={25} />
-        </div>
-        <div className='text-base flex justify-around'>
-          <div>
-            <h3 className='mb-2 text-center font-chirp-bold text-pri'>
-              Para ti
-            </h3>
-            <div className='h-1 rounded bg-primary w-14' />
+    <>
+      <div className='pt-3 border-[#2f3336] border-x-[1.5px]'>
+        <div className='px-4'>
+          <h2 className='text-normal font-chirp-bold text-pri mb-7 max-[500px]:hidden'>
+            Inicio
+          </h2>
+          <div className='fill-primary flex justify-center mb-6'>
+            <Twitter size={25} />
           </div>
-          <div>
-            <h3 className='text-secondary'>Siguiendo</h3>
+          <div className='text-base flex justify-around'>
+            <div>
+              <h3 className='mb-2 text-center font-chirp-bold text-pri'>
+                Para ti
+              </h3>
+              <div className='h-1 rounded bg-primary w-14' />
+            </div>
+            <div>
+              <h3 className='text-secondary'>Siguiendo</h3>
+            </div>
           </div>
         </div>
-      </div>
-      <div className='border-[#2f3336] pb-2 pt-5 px-4 border-y-[1.5px]'>
-        {/* <h2 className='mb-3'>¿Qué estas pensando?</h2> */}
-        <form
-          className='pl-[62px] break-words whitespace-pre-wrap'
-          onSubmit={handleSubmit(onSubmit)}
-        >
-          {/* {!user && (
+        <div className='border-[#2f3336] pb-2 pt-5 px-4 border-y-[1.5px]'>
+          {/* <h2 className='mb-3'>¿Qué estas pensando?</h2> */}
+          <form
+            className='pl-[62px] break-words whitespace-pre-wrap'
+            onSubmit={handleSubmit(onSubmit)}
+          >
+            {/* {!user && (
             <div className='relative w-full mb-6'>
               <input
                 placeholder='Nombre'
@@ -134,105 +140,140 @@ export default function AddPost({ user, currentUser, name }: Props) {
               )}
             </div>
           )} */}
-          <textarea
-            placeholder='¿Qué está pasando?'
-            className='outline-none w-full h-[52px] resize-none text-pri text-normal bg-black placeholder:text-normal placeholder:text-secondary'
-            {...register('description', {
-              required: true,
-              maxLength: 300,
-            })}
-          ></textarea>
-          {selectedImage && (
-            <div className='mb-2 relative'>
-              <span
-                onClick={() => setSelectedImage(null)}
-                className='absolute cursor-pointer leading-5 text-[17px] top-[5px] left-[5px] backdrop-blur-sm py-[5px] px-[10.5px] bg-[#0f1419bf] rounded-[50%]'
-              >
-                &times;
-              </span>
-              <img
-                className='rounded-2xl w-full'
-                src={URL.createObjectURL(selectedImage)}
-                alt='upload image'
-              />
-            </div>
-          )}
-          {errors.description?.type === 'required' && (
-            <p className='absolute text-xs text-red-700'>Obligatorio</p>
-          )}
-          {errors.description?.type === 'maxLength' && (
-            <p className='absolute text-xs text-red-700'>
-              Maximo 300 caracteres
-            </p>
-          )}
-          <div className='flex justify-between items-center'>
-            <div className='flex gap-1'>
-              <div className='hover:bg-imageHover rounded-[50%] w-[34px] h-[34px] flex items-center justify-center'>
-                <label htmlFor='submit' className='cursor-pointer'>
-                  <Images />
-                </label>
-                <input
-                  className='hidden'
-                  id='submit'
-                  type='file'
-                  onChange={(e) => {
-                    if (e.target.files?.length) {
-                      setSelectedImage(e.target.files[0])
-                    }
-                  }}
+            <textarea
+              placeholder='¿Qué está pasando?'
+              className='outline-none w-full h-[52px] resize-none text-pri text-normal bg-black placeholder:text-normal placeholder:text-secondary'
+              {...register('description', {
+                maxLength: 300,
+                onChange: (e) => {
+                  if (e.target.value.length >= 1) setIsDisabled(false)
+                  else setIsDisabled(true)
+                },
+              })}
+            ></textarea>
+            {selectedImage && (
+              <div className='mb-2 relative'>
+                <span
+                  onClick={() => setSelectedImage(null)}
+                  className='absolute cursor-pointer leading-5 text-[17px] top-[5px] left-[5px] backdrop-blur-sm py-[5px] px-[10.5px] bg-[#0f1419bf] rounded-[50%]'
+                >
+                  &times;
+                </span>
+                <img
+                  className='rounded-2xl w-full'
+                  src={URL.createObjectURL(selectedImage)}
+                  alt='upload image'
                 />
               </div>
-              <div className='flex items-center justify-center w-[34px] h-[34px]'>
-                <Gift />
+            )}
+            {errors.description?.type === 'maxLength' && (
+              <p className='absolute text-xs text-red-700'>
+                Maximo 300 caracteres
+              </p>
+            )}
+            <div className='flex justify-between items-center'>
+              <div className='flex gap-1'>
+                <div className='hover:bg-imageHover rounded-[50%] w-[34px] h-[34px] flex items-center justify-center'>
+                  <label htmlFor='submit' className='cursor-pointer'>
+                    <Images />
+                  </label>
+                  <input
+                    className='hidden'
+                    id='submit'
+                    type='file'
+                    onChange={(e) => {
+                      if (e.target.files?.length) {
+                        setSelectedImage(e.target.files[0])
+                      }
+                    }}
+                  />
+                </div>
+                <div className='flex items-center justify-center w-[34px] h-[34px]'>
+                  <Gift />
+                </div>
+                <div className='flex items-center justify-center w-[34px] h-[34px]'>
+                  <Survey />
+                </div>
+                <div className='flex items-center justify-center w-[34px] h-[34px]'>
+                  <Emoji />
+                </div>
+                <div className='flex items-center justify-center w-[34px] h-[34px]'>
+                  <Program />
+                </div>
+                <div className='flex items-center justify-center w-[34px] h-[34px]'>
+                  <Ubication />
+                </div>
               </div>
-              <div className='flex items-center justify-center w-[34px] h-[34px]'>
-                <Survey />
-              </div>
-              <div className='flex items-center justify-center w-[34px] h-[34px]'>
-                <Emoji />
-              </div>
-              <div className='flex items-center justify-center w-[34px] h-[34px]'>
-                <Program />
-              </div>
-              <div className='flex items-center justify-center w-[34px] h-[34px]'>
-                <Ubication />
+              <div>
+                <button
+                  className={` ${
+                    isDisabled ? 'bg-[#0e4e78]' : 'bg-primary text-white'
+                  }  text-base font-chirp-bold py-[6px] px-3 text-[#808080] rounded-[18px] w-[95px] hover: duration-200 ease-linear`}
+                >
+                  {isLoading ? (
+                    <Loader h='h-6' w='w-6' color='fill-white' />
+                  ) : (
+                    'Twittear'
+                  )}
+                </button>
               </div>
             </div>
-            <div>
-              <button className=' bg-[#0e4e78] text-base font-chirp-bold py-[6px] px-3 text-[#808080] rounded-[18px] w-[95px] hover: duration-200 ease-linear'>
-                {isLoading ? (
-                  <Loader h='h-6' w='w-6' color='fill-white' />
-                ) : (
-                  'Twittear'
-                )}
-              </button>
-            </div>
-          </div>
-        </form>
+          </form>
+        </div>
       </div>
       {isError && (
-        <div className='mb-3 border border-red-700 rounded p-4 relative'>
+        <div className='border-[1.5px] border-red-700 px-4 py-2 flex justify-between items-center'>
           <p className=' text-red-700'>Ha ocurrido un error</p>
-          <button
-            onClick={resetQuery}
-            className='absolute text-red-700 right-[20px] top-[16px]'
-          >
-            X
-          </button>
+          <div className='px-1 flex items-center'>
+            <button
+              onClick={() => {
+                resetQuery()
+                setError(false)
+              }}
+              className='py-1 px-3 text-2xl rounded-[50%] hover:bg-[#eff3f41a] duration-200 ease-linear text-red-700'
+            >
+              &times;
+            </button>
+          </div>
+        </div>
+      )}
+      {error && !name.isReady && (
+        <div className='border-[1.5px] border-red-700 px-4 py-2 flex justify-between items-center'>
+          <p className=' text-red-700'>
+            Elige un nombre o inicia sesión para twittear
+          </p>
+          <div className='px-1 flex items-center'>
+            <button
+              onClick={() => {
+                resetQuery()
+                setError(false)
+              }}
+              className='py-1 px-3 text-2xl rounded-[50%] hover:bg-[#eff3f41a] duration-200 ease-linear text-red-700'
+            >
+              &times;
+            </button>
+          </div>
         </div>
       )}
       {isSuccess && (
-        <div className='mb-3 border border-green-600 rounded p-4 relative'>
-          <p className=' text-green-600'>Comentario subido correctamente</p>
-          <button
-            onClick={resetQuery}
-            className='absolute text-green-600 right-[20px] top-[16px]'
-          >
-            X
-          </button>
+        <div className='border-[1.5px] border-primary px-4 py-2 flex justify-between items-center'>
+          <p className=' text-primary font-chirp-bold'>
+            Comentario subido correctamente
+          </p>
+          <div className='px-1 flex items-center'>
+            <button
+              onClick={() => {
+                resetQuery()
+                setError(false)
+              }}
+              className='py-1 px-3 text-2xl rounded-[50%] hover:bg-[#eff3f41a] duration-200 ease-linear text-primary'
+            >
+              &times;
+            </button>
+          </div>
         </div>
       )}
-    </div>
+    </>
   )
 }
 
