3e49656bb519b6e3b361b655bf6354a09f237af8
Facug03 <facundogonzales1550@gmail.com>
2023-02-27 14:12:49 -0300

    add env

--- front/src/helpers/firebase.ts
+++ front/src/helpers/firebase.ts
@@ -8,17 +8,17 @@ import {
 } from 'firebase/auth'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCe5jStXXeznhp6iVLynajVGMmZ6dZq9Q0',
+  apiKey: import.meta.env.VITE_API_KEY,
 
-  authDomain: 'red-social-45f01.firebaseapp.com',
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
 
-  projectId: 'red-social-45f01',
+  projectId: import.meta.env.VITE_PROJECT_ID,
 
-  storageBucket: 'red-social-45f01.appspot.com',
+  storageBucket: import.meta.env.VITE_STORAGE,
 
-  messagingSenderId: '683641337413',
+  messagingSenderId: import.meta.env.VITE_MESSASING,
 
-  appId: '1:683641337413:web:e1b88d5d762d2cfa76552c',
+  appId: import.meta.env.VITE_APP_ID,
 }
 
 const app = initializeApp(firebaseConfig)
